b09d6dfc6a9591ad57bb73da478b84593831727e
Antti Laine <anttlai@utu.fi>
2013-02-25 18:14:25 +0200

    added basic ui, no functionality, very rough

--- src/main/java/foo/MyVaadinApplication.java
+++ src/main/java/foo/MyVaadinApplication.java
@@ -15,12 +15,21 @@
  */
 package foo;
 
+import java.io.File;
+
+import com.vaadin.server.FileResource;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinService;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TabSheet;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.Reindeer;
 
 /**
  * The Application's "main" class Test comment! --Brigesh
@@ -29,24 +38,143 @@ import com.vaadin.ui.VerticalLayout;
 public class MyVaadinApplication extends UI {
 
     @Override
-    public void init(VaadinRequest request) {
-	final VerticalLayout layout = new VerticalLayout();
-	setContent(layout);
-	Button button = new Button("Click Me");
-	button.addClickListener(new Button.ClickListener() {
-	    public void buttonClick(ClickEvent event) {
-		layout.addComponent(new Label("Thank you for clicking!!!!"));
-	    }
-	});
-	Button jason = new Button("kick me");
-	jason.addClickListener(new Button.ClickListener() {
-	    public void buttonClick(ClickEvent event) {
-		layout.addComponent(new Label("Thank you for kicking!!!!"));
-	    }
-	});
-	layout.addComponent(button);
-	layout.addComponent(jason);
+    protected void init(VaadinRequest request) {
+        initLayout();
+    }
 
+    private void initLayout() {
+        VerticalLayout v = new VerticalLayout();
+        v.setStyleName(Reindeer.LAYOUT_BLUE);
+        v.setSpacing(false);
+        setContent(v);
+
+        Panel topBanner = initTopBanner();
+        v.addComponent(topBanner);
+        v.setComponentAlignment(topBanner, Alignment.TOP_CENTER);
+
+        // Panel userPanel = initUserPanel();
+        // v.addComponent(userPanel);
+        // v.setComponentAlignment(userPanel, Alignment.TOP_CENTER);
+
+        Panel middlePanel = initMiddlePanel();
+        v.addComponent(middlePanel);
+        v.setComponentAlignment(middlePanel, Alignment.MIDDLE_CENTER);
+
+        Panel bottomBanner = initBottomBanner();
+        v.addComponent(bottomBanner);
+        v.setComponentAlignment(bottomBanner, Alignment.BOTTOM_CENTER);
+    }
+
+    private Panel initMiddlePanel() {
+        HorizontalLayout h = new HorizontalLayout();
+
+        Panel sidePanel = initSidePanel();
+        h.addComponent(sidePanel);
+        h.setComponentAlignment(sidePanel, Alignment.MIDDLE_LEFT);
+
+        TabSheet contentTab = initContentTab();
+        contentTab.setSizeFull();
+        h.addComponent(contentTab);
+        h.setComponentAlignment(contentTab, Alignment.TOP_CENTER);
+
+        Panel middlePanel = new Panel();
+        middlePanel.setContent(h);
+
+        return middlePanel;
+    }
+
+    private Panel initTopBanner() {
+        Panel topBanner = new Panel();
+        topBanner.setWidth("100%");
+
+        String basepath = VaadinService.getCurrent().getBaseDirectory()
+                .getAbsolutePath();
+
+        FileResource resource = new FileResource(new File(basepath
+                + "/WEB-INF/images/topBannerPlaceholder.png"));
+
+        Embedded image = new Embedded("TopBanner placeholder image", resource);
+
+        VerticalLayout v = new VerticalLayout();
+        v.addComponent(image);
+        topBanner.setContent(v);
+
+        return topBanner;
+    }
+
+    private Panel initSidePanel() {
+        Panel sidePanel = new Panel();
+        sidePanel.setHeight("100%");
+
+        VerticalLayout vv = new VerticalLayout();
+        vv.setStyleName(Reindeer.LAYOUT_WHITE);
+
+        Label temporary = new Label("Side Panel Here");
+
+        Button login = new Button("Login");
+        Button register = new Button("Register");
+        Button createNewEvent = new Button("Create new event!");
+
+        vv.addComponent(temporary);
+        vv.addComponent(register);
+        vv.addComponent(login);
+        vv.addComponent(createNewEvent);
+
+        sidePanel.setContent(vv);
+
+        return sidePanel;
+    }
+
+    private Panel initUserPanel() {
+        Panel userPanel = new Panel();
+        userPanel.setWidth("100%");
+
+        HorizontalLayout v = new HorizontalLayout();
+        v.setStyleName(Reindeer.LAYOUT_WHITE);
+
+        Label temporary = new Label("User Panel Here");
+
+        Button viewMyEvents = new Button("My Events");
+        Button viewFriendEvents = new Button("Friend's Events");
+
+        v.addComponent(viewMyEvents);
+        v.addComponent(viewFriendEvents);
+        v.addComponent(temporary);
+        userPanel.setContent(v);
+
+        return userPanel;
+    }
+
+    private TabSheet initContentTab() {
+        TabSheet tabsheet = new TabSheet();
+        tabsheet.setSizeFull();
+        tabsheet.addTab(new Label("Contents of the first tab"), "My Events");
+        tabsheet.addTab(new Label("Contents of the first tab"),
+                "Friend's Events");
+        tabsheet.addTab(new Label("Contents of the first tab"),
+                "My Past events");
+
+        return tabsheet;
+    }
+
+    private Panel initBottomBanner() {
+        Panel bottomBanner = new Panel();
+        bottomBanner.setWidth("100%");
+
+        String basepath = VaadinService.getCurrent().getBaseDirectory()
+                .getAbsolutePath();
+
+        FileResource resource = new FileResource(new File(basepath
+                + "/WEB-INF/images/topBannerPlaceholder.png"));
+
+        Embedded image = new Embedded("BottomBanner placeholder image",
+                resource);
+
+        VerticalLayout v = new VerticalLayout();
+        v.addComponent(image);
+        bottomBanner.setContent(v);
+
+        return bottomBanner;
     }
 
 }
