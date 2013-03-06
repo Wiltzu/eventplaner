fbe8ac81cd23304ae34a6eb3182021526fbe2f31
Antti Laine <anttlai@utu.fi>
2013-03-06 17:22:51 +0200

    Added functionality to voting buttons

--- src/main/java/foo/EventWindow.java
+++ src/main/java/foo/EventWindow.java
@@ -4,6 +4,8 @@ import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.data.util.filter.Compare.Equal;
 import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
@@ -12,6 +14,7 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
 import foo.domain.Activity;
+import foo.domain.Activity.Thumb;
 import foo.domain.MyEvent;
 import foo.domain.User;
 
@@ -21,6 +24,7 @@ public class EventWindow extends Window {
     private JPAContainer<MyEvent> events;
     private MyEvent myEvent;
     private JPAContainer<Activity> activities;
+    private Table tblActivities;
 
     public EventWindow(MyEvent myEvent, JPAContainer<Activity> activities) {
         super(myEvent.getName());
@@ -55,13 +59,12 @@ public class EventWindow extends Window {
         Label lblUserList = new Label(userlist, ContentMode.PREFORMATTED);
         v.addComponent(lblUserList);
 
-        // table for showing the activities related to the event 
+        // table for showing the activities related to the event
         activities.addContainerFilter(new Equal("event", myEvent));
         activities.applyFilters();
-        Table tblActivities = new Table("Activities", activities);
+        tblActivities = new Table("Activities", activities);
         tblActivities.setSelectable(true);
         tblActivities.setWidth("100%");
-       
 
         // shrunk table to contents
         tblActivities.setPageLength(0);
@@ -100,13 +103,62 @@ public class EventWindow extends Window {
         HorizontalLayout h = new HorizontalLayout();
 
         Button btnUpVote = new Button("Good idea");
+        btnUpVote.addClickListener(new ClickListener() {
+            Object tblIndex;
+            Activity selectedActivity;
+            User currentUser;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                try {
+                    tblIndex = tblActivities.getValue();
+                    selectedActivity = activities.getItem(tblIndex).getEntity();
+                    // get current user
+                    currentUser = (User) getParent().getUI().getSession()
+                            .getAttribute("user");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (currentUser != null && selectedActivity != null) {
+                    selectedActivity.vote(Thumb.UP, currentUser);
+                    // add user to list of people who have voted for this
+                    // activity
+                    selectedActivity.getVoters().add(currentUser);
+                }
+            }
+        });
         h.addComponent(btnUpVote);
 
         Button btnDownVote = new Button("Bad idea");
+        btnDownVote.addClickListener(new ClickListener() {
+            Object tblIndex;
+            Activity selectedActivity;
+            User currentUser;
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                try {
+                    tblIndex = tblActivities.getValue();
+                    selectedActivity = activities.getItem(tblIndex).getEntity();
+                    // get current user
+                    currentUser = (User) getParent().getUI().getSession()
+                            .getAttribute("user");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (currentUser != null && selectedActivity != null) {
+                    selectedActivity.vote(Thumb.DOWN, currentUser);
+                    // add user to list of people who have voted for this
+                    // activity
+                    selectedActivity.getVoters().add(currentUser);
+                }
+            }
+        });
+
         h.addComponent(btnDownVote);
 
         p.setContent(h);
         return p;
     }
-
 }
